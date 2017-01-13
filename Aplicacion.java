ee16855b84fdbaabb56daaa3735724eb14fb8780
juliorosero <david-kamina@hotmail.com>
2017-01-13 11:08:10 -0500

    Add files via upload

---
+++ Aplicacion.java
@@ -0,0 +1,39 @@
+ class Aplicacion {
+
+	public static void main(String[] args)
+	{
+				 
+		SLinkedList libro = new SLinkedList(); 
+		
+		 if (libro.estaVacia())
+			 System.out.println("Lista sin llenar");
+		 
+		 libro.insertarFinal("El Anticristo");
+		 libro.insertarFinal("Las venas abiertas de America Latina");
+		 libro.insertarFinal("Una breve histotairia del cpital");
+		 libro.insertarFinal("Manuela");
+		 libro.insertarFinal("Mitos y Leyendas del Ecuador");
+		 libro.insertarFinal("Breve Historia republicana del Ecuador");
+		 libro.insertarFinal("El universo en una cascara de nuez");
+		 libro.insertarFinal("Polvo de Estrella");
+		 
+		 libro.imprimirLista();
+		 System.out.println("\nLa lista de libros esta compuesta de :  "+libro.tamaño()+" y los libros son:");
+		 
+		 libro.eliminarpos(3);
+		 
+		 libro.imprimirLista();
+		 System.out.println("\nLa lista de libros esta compuesta de :  "+libro.tamaño()+" y los libros son:");
+		 
+		 libro.localizar(3);
+		 
+		 libro.vaciar();
+		 
+		 if (libro.estaVacia())
+			 System.out.println("\nLista sin llenar");
+		 
+		 System.out.println("La lista de libros esta compuesta de :  "+libro.tamaño()+" y los libros son:");
+
+	}
+
+}
---
+++ SLinkedList.java
@@ -0,0 +1,126 @@
+public class SLinkedList 
+{
+	protected Node head;
+	protected long size;
+	
+	
+	public void SLinkedlist() 
+	{
+	head = null;
+	size= 0; 
+
+	}
+	 public boolean estaVacia(){
+		 
+		 return head==null;
+		 
+	 }
+
+	 public void insertaralInicio(String a){
+		 if (estaVacia()) 
+			  head= new Node(a);
+		 else 
+			  head = new Node(a,head);
+		 
+		 
+	 }
+	 
+	 public void insertarFinal(String a){
+
+	     Node auxiliar = new Node(a);
+	     Node inicio;
+
+	     if (estaVacia()) {
+	         insertaralInicio(a);
+	     }else {
+	         inicio = head;         
+	         while(inicio.getNext() != null){
+	             inicio=inicio.getNext();
+	         }              
+	         inicio.setNext(auxiliar);
+	     }
+	 }
+	 
+	 public  void vaciar(){
+		  head=null;
+	 }
+	 
+	 public int tamaño()
+	 {
+	     Node auxiliar;
+	     size=0;
+	     auxiliar = head;
+
+	     while(auxiliar != null)
+	     {
+	         size++;
+	         auxiliar = auxiliar.getNext();
+	     }
+	     return (int) size;
+
+	 }
+	 
+	public void localizar(int posicion){
+		 
+	     Node auxiliar= head;
+	     int contador=1;
+
+	     if(posicion<1 || posicion>=tamaño()){
+	         System.out.println("posicion no encontrada");
+	     }else{
+	         while(auxiliar!=null){
+	             if (posicion == contador){
+	                 System.out.println("el elemento es : "+auxiliar.getElement()
+	                 					+": en la posicion "+ posicion);
+	                 auxiliar=null;
+	                 }
+	                 
+	             else{                 
+	                 auxiliar=auxiliar.getNext();
+	                 contador++;
+	             }
+	         }
+	     }
+	 }
+	 public void eliminarpos(int posicion){
+		 
+	     Node auxiliar= head;
+	     Node anterior=null;
+	     int contador=1;
+
+	     if(posicion<1 || posicion>=tamaño()){
+	         System.out.println("posicion no encontrada");
+	     }else{
+	         while(auxiliar!=null){
+	             if (posicion == contador){
+	                 if (anterior==null){
+	                     head = head.getNext();
+	                 }else {
+	                     anterior.setNext(auxiliar.getNext());
+	                 }
+	                 auxiliar=null;
+	             }else{
+	                 anterior=auxiliar;
+	                 auxiliar=auxiliar.getNext();
+	                 contador++;
+	             }
+	         }
+	     }
+	 }
+	 
+	 public void imprimirLista(){
+	     System.out.println("La lista es:  ");
+	 
+	     
+	     Node aux= head;
+	     
+	     while(aux!=null){
+	         System.out.println(aux.getElement());
+	         aux=aux.getNext();
+	     }
+	     
+	 }
+
+	
+	
+}
